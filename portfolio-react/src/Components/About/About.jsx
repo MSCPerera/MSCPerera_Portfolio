4c9760378c501c0191932981a4d1dd5ed21bc1b8
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-04 23:55:41 +0530

    Update About section

--- portfolio-react/src/Components/About/About.jsx
+++ portfolio-react/src/Components/About/About.jsx
@@ -10,12 +10,12 @@ const About = () => {
         </div>
         <div className="about-sections">
             <div className="about-left">
-                <img src={profile_img1} alt="" />
+                <img src={profile_img1} alt="" className='about-img'/>
             </div>
             <div className="about-right">
                 <div className="about-para">
                     <p>I am a third-year student pursuing a BSc (Hons) degree in Computing and Information Systems at Sabaragamuwa University in Sri Lanka, seeking a software engineering internship with experience in ReactJS, Node.js, Express.js, MongoDB, MySQL, HTML, CSS, JavaScript, C, and C#.</p>
-                    <p>I am passionate about creating dynamic, responsive user interfaces. I am eager to contribute my skills and dedication to the workplace.</p>
+                    
                 </div>
                 <div className="about-skills">
                     <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"60%"}}/></div>
@@ -24,8 +24,23 @@ const About = () => {
                     <div className="about-skill"><p>Node JS</p><hr style={{width:"50%"}}/></div>
                     <div className="about-skill"><p>Express JS</p><hr style={{width:"50%"}}/></div>
                     <div className="about-skill"><p>MongoDB</p><hr style={{width:"70%"}}/></div>
-                    <div className="about-skill"><p>MySQL</p><hr style={{width:"60%"}}/></div>
                 </div>
+            </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>Top 15 finalists</h1>
+                <p>Nexora 1.0 Inter University Datathon, NSBM Green University</p>
+            </div>
+            <hr/>
+            <div className="about-achievement">
+                <h1>Secon Round Qualification</h1>
+                <p>DevThon 2.0, University of Moratuwa</p>
+            </div>
+            <hr/>
+            <div className="about-achievement">
+                <h1>Participation</h1>
+                <p>IDEALIZE 2025, University of Moratuwa</p>
             </div>
         </div>
     </div>
