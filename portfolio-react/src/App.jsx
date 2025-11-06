c383ba5257d8cd4080b0879dfdf4949fd857bf5b
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-06 23:55:23 +0530

    Create services page

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
     </div>
   )
 }
---
+++ portfolio-react/src/Components/Services/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Services.css'
+
+const Services = () => {
+  return (
+    <div className='services'>
+        <div className="services-title">
+            <h1>Mys Services</h1>
+        </div>
+        <div className="services-container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Services
