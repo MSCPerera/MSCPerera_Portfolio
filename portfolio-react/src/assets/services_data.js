04056d7fa324bafad5315723a4b410f39cb3c592
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-09 22:10:48 +0530

    Update works

--- portfolio-react/src/assets/services_data.js
+++ portfolio-react/src/assets/services_data.js
@@ -2,17 +2,17 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Web development is the process of creating and maintaining websites to make them functional, user-friendly, and visually appealing. I am creating creative and responsive software and applications for consumers' requirements."
     },
         {
         s_no:"02",
         s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"As a software developer with experience in digital marketing, I combine technical skills with creative strategies to run effective online campaigns. I focus on using data-driven insights, and SEO techniques to reach target audiences and boost brand visibility."
     },
     {
         s_no:"03",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"As a software developer and digital marketer, I manage my own online electronic selling shop through various digital platforms. I focus on development and marketing allows me to create a smooth online shopping experience and effectively grow my business in the digital marketplace."
     },
 ]
  
